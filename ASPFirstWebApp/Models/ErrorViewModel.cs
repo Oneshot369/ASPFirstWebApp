7c93defaaf9aaecdb50d484d0604309877d3447f
Oneshot369 <joshcpeck4@gmail.com>
2023-09-08 08:29:22 -0700

    Add project files.

---
+++ ASPFirstWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPFirstWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
